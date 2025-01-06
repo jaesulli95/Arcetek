dd10a12b98f612b4eb0c653fd90e3c0b7a6b5456
Joshua Sullivan <jsulli0527@gmail.com>
2025-01-06 01:06:40 -0500

    Added GameStateBase and ignoring content stylizedforestenvironment
    Added GameStateBase and ignoring content stylizedforestenvironment

---
+++ Source/Arcetek/Public/GameStates/ArcetekGameStateBase.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameStateBase.h"
+#include "ArcetekGameStateBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARCETEK_API AArcetekGameStateBase : public AGameStateBase
+{
+	GENERATED_BODY()
+public:
+	AArcetekGameStateBase();
+};
